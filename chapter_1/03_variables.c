0f2ac4c876ae86ee540c58715c504c6f24626a8a
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-17 16:30:02 +0000

    Auto commit on Tue Feb 17 16:30:02 UTC 2026

--- chapter_1/03_variables.c
+++ chapter_1/03_variables.c
@@ -5,7 +5,9 @@ int main()
 	float b = 1.4;
 	char c = 'a'; //always written in single quoat
 
-printf("The values a,b and c is:%d %f %c \n",a,b,c);
+printf("The value of a is:%d\n",a);
+printf("The value of b is:%f\n",b);
+printf("The value of c is:%c\n",c);
 
 	return 0;
 }
---
+++ chapter_1/06_escape_sequence.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	char ch = 'a';
+	
+// \n represents new line ex:
+
+printf("Hello Anshu \n nice\n");
+
+// \" represnts double quoat ex:
+
+printf("hello Anshu\" nice\n");
+
+// \t represents tab space/horizontal tab ex:
+
+printf("Hello\tworld\n");
+
+// \\ represents \ ex:
+
+printf("Hello \\ World/n");
+
+return 0;
+
+}
