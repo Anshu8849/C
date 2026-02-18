8232fd001b5693c27c55cc2d8f03e5434b0b7428
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-18 17:05:17 +0530

    commit

--- chapter_1_practice_set/Q2.c
+++ chapter_1_practice_set/Q2.c
@@ -1,7 +1,7 @@
 //calculate the area of circle and modify the same program to calculate the volume of cylinder given its radius and hight.
 
 //1.calculate the area of circle.
-/*
+
 
 #include<stdio.h>
 void main()
@@ -12,10 +12,10 @@ printf("Area of circle with radius:%d is %f\n",r,3.14*r*r);
 
 }
 
-*/
 
 //2.calculate the volume of given radius and hight.
 
+/*
 #include<stdio.h>
 void main ()
 {
@@ -23,6 +23,7 @@ void main ()
 	int r = 4;
 	int hight = 10;
 
-Printf("The volume of the cylinder is with radius %d and hight %d is %f \n",r,3.14*r*r*hight);
+printf("The volume of the cylinder with radius is %d and hight %d is %f \n",r,hight,3.14*r*r*hight);
 
 }
+*/
