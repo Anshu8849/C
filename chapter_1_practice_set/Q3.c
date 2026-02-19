bc801ca5c35b59dce3b2a16e66da3695e7a8e000
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-19 12:30:03 +0000

    Auto commit on Thu Feb 19 12:30:03 UTC 2026

---
+++ chapter_1_practice_set/Q3.c
@@ -0,0 +1,17 @@
+//write a program to convert celsius to fahrenheit.
+
+
+#include<stdio.h>
+int main()
+{
+
+	float = celsius;
+	
+printf("Enter a value of celsius:\n");
+scanf("%f\n",&celsius);
+
+printf("\nfahrenheit value is:%f\n"celsius,F=(celsius *9/5)+32);
+
+	return 0;
+}
+
