e1f90c9786c51b4a3cc0d598e2b87903ba1a9f95
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-22 12:30:04 +0000

    Auto commit on Sun Feb 22 12:30:04 UTC 2026

---
+++ chapter_2/02_arithmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{	
+	int a,b;
+	printf("Enter value of int a:\n");
+	scanf("%d",&a);
+
+	printf("Enter value of int b:\n");
+	scanf("%d",&b);
+
+	int subtraction = a - b;
+
+	printf("subtraction of two values is:%d\n",a - b);
+
+	return 0;
+
+}
