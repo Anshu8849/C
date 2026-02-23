eab008a6f61dffcc87d63c104308ba777c11cea6
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-23 13:00:01 +0000

    Auto commit on Mon Feb 23 13:00:01 UTC 2026

---
+++ chapter_2/1_assignment_op/01_assignment_op.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+
+	int a,b,c; // declaration of variables
+ 
+	a = 5;   //assign value to integer a
+	b = 6;   //assign value to integer b
+
+	c = a; // assign a value to a integer c
+
+printf("value of a=%d b=%d c=%d/n",a,b,c);
+
+}
---
+++ chapter_2/2_arithmetic_op/06_arithmetic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+	int i = 10,j = 20,k = 30;
+	printf("%d",i+j*k-5/5);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
