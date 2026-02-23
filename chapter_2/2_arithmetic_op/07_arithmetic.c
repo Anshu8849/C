ae9588275790662f95bc03c67a3f9a2acc0ecb84
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-23 13:30:02 +0000

    Auto commit on Mon Feb 23 13:30:02 UTC 2026

---
+++ chapter_2/2_arithmetic_op/07_arithmetic.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main ()
+{
+	int a=50,b=10,c=5,d=2,e=5;
+	printf("%d\n",a-b+c*d/e);
+	return 0;
+}
---
+++ chapter_2/2_arithmetic_op/08_arithmetic.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=8,b=4,c=3,d=6,e=2;
+	printf("%d",(a+b)*c-d/e);
+	return 0;
+
+}
