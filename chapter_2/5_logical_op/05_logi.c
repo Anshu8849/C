8f44d8b2ac620253ec22a51d7ffb6c1b26c93e1e
Anshu8849 <patelanshu1102004@gmail.com>
2026-02-25 21:46:26 +0530

    commit 25/02

---
+++ chapter_2/5_logical_op/05_logi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int i=10,j=20,k=30,l=40,r;
+
+	printf("i=%d j=%d k=%d l=%d r=%d\n",i,j,k,l,r);
+
+	r = i && (j=200) || (k=300) && (l=400);
+
+	printf("i=%d j=%d k=%d l=%d r=%d\n",i,j,k,l,r);
+
+}
+
+
